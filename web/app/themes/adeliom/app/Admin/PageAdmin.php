c01e0fd1627426469e591216a3ae543a7610bb64
Antoine WAAG <antoine.waag@adeliom.com>
2022-11-10 09:43:11 +0100

    chore: open 1.1-dev

--- web/app/themes/adeliom/app/Admin/PageAdmin.php
+++ web/app/themes/adeliom/app/Admin/PageAdmin.php
@@ -3,7 +3,20 @@
 namespace App\Admin;
 
 use Adeliom\Lumberjack\Admin\AbstractAdmin;
+use Adeliom\Lumberjack\Admin\Fields\Buttons\ButtonField;
+use Adeliom\Lumberjack\Admin\Fields\Medias\FileField;
+use Adeliom\Lumberjack\Admin\Fields\Medias\ImageField;
+use Adeliom\Lumberjack\Admin\Fields\Medias\MediaField;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\ContentTab;
+use Adeliom\Lumberjack\Admin\Fields\Layout\LayoutField;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\LayoutTab;
 use Adeliom\Lumberjack\Admin\Fields\Tabs\MediaTab;
+use Adeliom\Lumberjack\Admin\Fields\Settings\SettingsField;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\SettingsTab;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\TabField;
+use Adeliom\Lumberjack\Admin\Fields\Typography\HeadingField;
+use Adeliom\Lumberjack\Admin\Fields\Typography\TextareaField;
+use Adeliom\Lumberjack\Admin\Fields\Typography\WysiwygField;
 use Traversable;
 
 /**
@@ -45,7 +58,34 @@ class PageAdmin extends AbstractAdmin
      */
     public static function getFields(): Traversable
     {
-        yield MediaTab::make()->media();
+
+        yield from ContentTab::make()->fields([
+            HeadingField::make()->tag(),
+            WysiwygField::make()->default(),
+            ButtonField::make()->group()
+        ]);
+
+        yield from TabField::make("Description", "desc_tab")->fields([
+            TextareaField::make(),
+            FileField::make()->pdf(),
+            ImageField::make()->ratio(150, 200)
+        ]);
+
+        yield from LayoutTab::make()->fields([
+            LayoutField::darkMode(),
+            LayoutField::margin([
+                LayoutField::MARGIN_TOP_REMOVE,
+                LayoutField::MARGIN_BOTTOM_REMOVE,
+            ])
+        ]);
+
+        yield from MediaTab::make()->fields([
+            MediaField::make()
+        ]);
+
+        yield from SettingsTab::make()->fields([
+            SettingsField::anchor()
+        ]);
     }
 
     /**
